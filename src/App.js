929f2f81de2b66cde932cda63c8324b2de556db4
Silas Burger <ybsilas@gmail.com>
2019-04-09 12:52:36 -0700

    storing highscore in localstorage

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ class App extends Component {
     super(props);
     this.state = {
       hasLost: false,
-      highscore: 0,
+      highscore: null,
       currScore: 0,
     }
     this.toggleLost = this.toggleLost.bind(this);
     this.addScore = this.addScore.bind(this);
+  }
+
+  componentDidMount() {
+    let highscore = localStorage.getItem('highscore');
+    this.setState({highscore});
   }
 
   static defaultProps = {
@@ -23,7 +28,10 @@ class App extends Component {
     let highscore = this.state.highscore;
     let currScore = 0;
     if(this.state.hasLost === false) currScore = this.state.currScore;
-    if(this.state.currScore >= this.state.highscore) highscore = this.state.currScore;
+    if(this.state.currScore >= this.state.highscore) {
+      highscore = this.state.currScore;
+      localStorage.setItem('highscore', highscore);
+    }
     this.setState((st)=>({hasLost:!st.hasLost, highscore, currScore}));
   }
   
@@ -34,7 +42,7 @@ class App extends Component {
   render() {
     return (
       <div className="game-container" style={{height: this.props.height, width: this.props.width}}>
-        <div className="high-score">Highscore: {this.state.highscore}</div>
+        <div className="high-score">Highscore: {this.state.highscore ? this.state.highscore : 0}</div>
         <div className="current-score">Current Score: {this.state.currScore}</div>
         {this.state.hasLost ? <button className="restart-button" onClick={this.toggleLost}>Restart</button> : <Board height={this.props.height} width={this.props.width} currScore={this.props.currScore} addScore={this.addScore} toggleLost={this.toggleLost} />}
       </div>
